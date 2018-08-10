d5e3058e74b49827123e35a9a45b85a39dfad7ce
adrian <adrian.sebuliba1@gmail.com>
2018-08-10 11:43:08 +0300

    feat(routing): setup routing and guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,64 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule,  Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { ItemsComponent } from './components/items/items.component';
 import { AddCategoryComponent } from './components/add-category/add-category.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
+import { NavigateAwayGuardService } from './services/navigate-away-guard.service';
+import { CategoriesResolverService } from './services/categories-resolver.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
-{
-  path: 'login',
-  component: LoginComponent
-},
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthGuardService]
+  },
   {
     path: 'register',
     component: RegisterComponent
   },
   {
     path: 'categories',
-    component: CategoriesComponent
+    component: CategoriesComponent,
+    resolve: { categories: CategoriesResolverService },
+    canActivate: [AuthGuardService]
   },
   {
-    path: 'items',
-    component: ItemsComponent
+    path: 'add-category',
+    component: AddCategoryComponent,
+    canDeactivate: [NavigateAwayGuardService],
+    canActivate: [AuthGuardService]
   },
   {
-    path: 'add-catgory',
-    component: AddCategoryComponent
+    path: 'add-category/:id',
+    component: AddCategoryComponent,
+    canDeactivate: [NavigateAwayGuardService],
+    canActivate: [AuthGuardService]
+  },
+
+  {
+    path: 'categories/:id',
+    component: ItemsComponent,
+    canActivate: [AuthGuardService],
   },
   {
-    path: 'add-item',
-    component: AddItemComponent
+    path: 'categories/:id/add-item',
+    component: AddItemComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -13,6 +12,16 @@ import { AddCategoryComponent } from './components/add-category/add-category.com
 import { AddItemComponent } from './components/add-item/add-item.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { RouterModule } from '../../node_modules/@angular/router';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenService } from './services/token.service';
+import { DataService } from './services/data.service';
+import { HttpInterceptorService } from './services/http-interceptor.service';
+import { NavigateAwayGuardService } from './services/navigate-away-guard.service';
+import { CategoriesResolverService } from './services/categories-resolver.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthSingletonService } from './services/auth-singleton.service';
 
 
 @NgModule({
@@ -25,14 +34,25 @@ import { RouterModule } from '../../node_modules/@angular/router';
     ItemsComponent,
     AddCategoryComponent,
     AddItemComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
     FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    TokenService,
+    DataService,
+    NavigateAwayGuardService,
+    CategoriesResolverService,
+    AuthGuardService,
+    AuthSingletonService,
+    { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true, }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
